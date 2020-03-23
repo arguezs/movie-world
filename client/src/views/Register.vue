7c62c742ab8715cdebbebfb692c63478173167af
Alejandro <rs.alejandro.93@gmail.com>
2020-03-23 11:32:01 +0100

    Bare, non-functioning version of register page

---
+++ client/src/views/Register.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Register</h1>
+  <input type="email" placeholder="email">
+  <input type="password" placeholder="Password">
+  <button>Submit</button>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
