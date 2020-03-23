241ec79188bda2ea371f36f8e37afda47d51c0b2
Alejandro <rs.alejandro.93@gmail.com>
2020-03-23 11:34:41 +0100

    Template fixed

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -1,8 +1,10 @@
 <template>
-  <h1>Register</h1>
-  <input type="email" placeholder="email">
-  <input type="password" placeholder="Password">
-  <button>Submit</button>
+    <div>
+        <h1>Register</h1>
+        <input type="email" placeholder="email">
+        <input type="password" placeholder="Password">
+        <button>Submit</button>
+    </div>
 </template>
 
 <script>
