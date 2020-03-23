49113a57bdf617e9b85d67026b851e087801bff9
Alejandro <rs.alejandro.93@gmail.com>
2020-03-23 12:15:54 +0100

    Added some data to model

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -1,15 +1,20 @@
 <template>
     <div>
         <h1>Register</h1>
-        <input type="email" placeholder="email"><br>
-        <input type="password" placeholder="Password"><br>
+        <input type="email" placeholder="email" v-model="email"><br>
+        <input type="password" placeholder="Password" v-model="password"><br>
         <button>Submit</button>
     </div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            email: '',
+            password: ''
+        }
+    }
 }
 </script>
 
