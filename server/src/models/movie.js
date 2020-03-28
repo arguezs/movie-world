bb5695ce0e356a2d7950ffa82447e8086856b1ff
Alejandro <rs.alejandro.93@gmail.com>
2020-03-28 12:38:19 +0100

    Fixed Movie model

--- server/src/models/movie.js
+++ server/src/models/movie.js
@@ -3,13 +3,14 @@ module.exports = (sequelize, DataTypes) => {
   const Movie = sequelize.define('Movie', {
     title: DataTypes.STRING,
     duration: DataTypes.INTEGER,
+    year: DataTypes.INTEGER,
     rating: DataTypes.STRING,
     trailer: DataTypes.STRING,
     poster: DataTypes.STRING,
     director: DataTypes.STRING,
     cast: DataTypes.STRING,
     genre: DataTypes.STRING,
-    sinopsis: DataTypes.STRING
+    sinopsis: DataTypes.TEXT
   }, {});
   /* Movie.associate = function(models) {
     // associations can be defined here
