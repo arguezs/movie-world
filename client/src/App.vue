255dc865e62dbe0cdee614710c5849bc5b4ccc3e
Alejandro <rs.alejandro.93@gmail.com>
2020-03-28 11:01:27 +0100

    Added movie route to main navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -27,14 +27,11 @@
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-toolbar-items>
+        <v-btn
+          text
+          :to="{name: 'Movies'}">Películas</v-btn>
+      </v-toolbar-items>
     </v-app-bar>
 
     <v-content>
