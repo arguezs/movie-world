97625bf9baac9fbb81f58540c762c651376e4fde
Alejandro <rs.alejandro.93@gmail.com>
2020-03-31 16:51:05 +0200

    Created association between Session and Seat

--- server/src/models/session.js
+++ server/src/models/session.js
@@ -6,6 +6,8 @@ module.exports = (sequelize, DataTypes) => {
   Session.associate = function(models) {
     Session.belongsTo(models.Theater, {as: 'theater'})
     Session.belongsTo(models.Movie, {as: 'movie'})
+    
+    Session.belongsToMany(models.Seat, {through: 'SessionSeat'})
   }
 
   return Session
