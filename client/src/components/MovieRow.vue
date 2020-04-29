5d22a9c0798ff35f4d73f130b7a72e11661c105c
Alejandro <rs.alejandro.93@gmail.com>
2020-04-29 15:59:19 +0200

    Created component for a row of movie previews

---
+++ client/src/components/MovieRow.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container>
+    <v-row v-if="movies.length">
+      <v-col
+        v-for="movie in movies"
+        :key="movie.id">
+        <movie-preview movie="movie" />
+      </v-col>
+    </v-row>
+    <v-row v-else>
+      <slot name="empty-message"></slot>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import MoviePreview from './MoviePreview'
+
+export default {
+  props: {
+    movies: []
+  },
+  components: { MoviePreview }
+}
+</script>
+
+<style>
+
+</style>
