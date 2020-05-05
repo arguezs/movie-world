957ed4218e851eb24652f1baa16ff5971b9f1818
Alejandro <rs.alejandro.93@gmail.com>
2020-05-05 12:20:23 +0200

    Added new libraries to main app

--- server/src/app.js
+++ server/src/app.js
@@ -3,12 +3,24 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 
+const cookieSession = require('cookie-session')
+const passport = require('passport')
+// const LocalStrategy = require('passport-local').Strategy
+
 const {sequelize} = require('./models')
 
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
+
+app.use(cookieSession({
+    name: 'mw-session',
+    keys: ['mw-auth-key'],
+    maxAge: 24 * 60 * 60 * 1000 //24h
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
 require('./routes')(app)
 
