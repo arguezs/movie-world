0178c68572d819160ef52854ca48efa5bc3c76ea
Alejandro <rs.alejandro.93@gmail.com>
2020-05-09 10:43:59 +0200

    Added Vuex and created Store

--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
+import Vuex from 'vuex'
 
 Vue.config.productionTip = false
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    user: null
+  },
+  mutations: {
+    updateUser (state, user) {
+      state.user = user
+    }
+  }
+})
 
 new Vue({
   router,
