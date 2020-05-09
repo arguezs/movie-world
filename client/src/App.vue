b733191d15a841f24b6aaf16c422215aad86e84a
Alejandro <rs.alejandro.93@gmail.com>
2020-05-09 10:58:36 +0200

    Moved main header to single component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,47 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-toolbar-items>
-        <v-btn
-          text
-          exact
-          :to="{name: 'Home'}">Inicio</v-btn>
-        <v-btn
-          text
-          exact
-          :to="{name: 'Movies'}">Películas</v-btn>
-        <v-btn
-          text
-          exact
-          :to="{name: 'Sessions'}">Sesiones</v-btn>
-      </v-toolbar-items>
-    </v-app-bar>
+    <main-header />
 
     <v-content>
       <router-view></router-view>
@@ -50,10 +9,11 @@
 </template>
 
 <script>
+import MainHeader from './components/MainHeader'
 
 export default {
   name: 'App',
-
+  components: { MainHeader },
   data: () => ({
     //
   }),
---
+++ client/src/components/MainHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app-bar
+    app
+    color="primary"
+    dark>
+
+    <v-toolbar-items>
+      <v-btn
+        text
+        exact
+        :to="{name: 'Home'}">Inicio</v-btn>
+      <v-btn
+        text
+        exact
+        :to="{name: 'Movies'}">Películas</v-btn>
+      <v-btn
+        text
+        exact
+        :to="{name: 'Sessions'}">Sesiones</v-btn>
+    </v-toolbar-items>
+
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
