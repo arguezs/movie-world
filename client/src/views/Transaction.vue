0e3965e7821df72fc99d7d3da8c90c08b28b8ed2
Alejandro <rs.alejandro.93@gmail.com>
2020-05-14 13:03:58 +0200

    Ticket selector now functional

--- client/src/views/Transaction.vue
+++ client/src/views/Transaction.vue
@@ -37,17 +37,35 @@
             <v-row class="text-center">
               <v-col>Selecciona cuántas entradas quieres</v-col>
             </v-row>
-            <v-row>
-              <v-col>Entradas</v-col>
-              <v-col>-</v-col>
-              <v-col>0</v-col>
-              <v-col>+</v-col>
-              <v-col>Precio</v-col>
-              <v-col>Total</v-col>
+            <v-row align="center" justify="center" class="text-center">
+              <v-col cols="3">Entradas</v-col>
+              <v-col cols="2">Precio</v-col>
+              <v-col cols="2">Total</v-col>
             </v-row>
-            <v-row>
-              <v-btn @click="step = 2">Continuar</v-btn>
-              <v-btn to="/">Cancelar</v-btn>
+            <v-row align="center" justify="center" class="text-center">
+              <v-col cols="1">
+                <v-btn
+                  fab outlined x-small
+                  :disabled="tickets==0"
+                  @click="tickets-=1"><v-icon>remove</v-icon></v-btn>
+              </v-col>
+              <v-col cols="1">{{ tickets }}</v-col>
+              <v-col cols="1">
+                <v-btn
+                  fab outlined x-small
+                  :disabled="tickets==10"
+                  @click="tickets+=1"><v-icon>add</v-icon></v-btn>
+              </v-col>
+              <v-col cols="2">{{ price.toFixed(2) }} €</v-col>
+              <v-col cols="2">{{ totalPrice.toFixed(2) }} €</v-col>
+            </v-row>
+            <v-row class="mt-6" justify="end">
+              <v-col cols="auto">
+                <v-btn to="/">Cancelar</v-btn>
+              </v-col>
+              <v-col cols="auto">
+                <v-btn color="primary" @click="step = 2">Continuar</v-btn>
+              </v-col>
             </v-row>
           </v-container>
         </v-stepper-content>
@@ -81,7 +99,14 @@ export default {
   data () {
     return {
       step: 1,
-      session: null
+      session: null,
+      tickets: 0,
+      price: 9.5
+    }
+  },
+  computed: {
+    totalPrice () {
+      return this.tickets * this.price
     }
   },
   async mounted () {
