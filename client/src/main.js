98c359059eda5b619a7993546a4a3eb4138d2523
Alejandro <rs.alejandro.93@gmail.com>
2020-05-19 11:47:39 +0200

    Added Vuelidate

--- client/src/main.js
+++ client/src/main.js
@@ -4,12 +4,14 @@ import router from './router'
 import vuetify from './plugins/vuetify'
 import Vuex from 'vuex'
 import VueCookies from 'vue-cookies'
+import Vuelidate from 'vuelidate'
 
 import AuthenticationService from '@/services/AuthenticationService'
 
 Vue.config.productionTip = false
 Vue.use(Vuex)
 Vue.use(VueCookies)
+Vue.use(Vuelidate)
 
 const store = new Vuex.Store({
   state: {
