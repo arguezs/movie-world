901cc89a175e220dbf1c8710f46fa2672f113e91
Alejandro <rs.alejandro.93@gmail.com>
2020-05-19 12:58:26 +0200

    Client side registration validation

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -12,6 +12,9 @@
       dense
       :type="result.success?'success':'error'">
       <span v-if="result.success">Registro completo</span>
+      <ul v-else-if="result.invalid">
+        <li v-for="message in result.message" :key="message">{{message}}</li>
+      </ul>
       <span v-else>Error al registrar</span>
     </v-alert>
 
@@ -26,6 +29,11 @@
         v-model="user.password"
         type="password"
         required />
+      <v-text-field
+        label="Repetir contraseña"
+        v-model="repeatPassword"
+        type="password"
+        required />
       <v-btn
         @click="register">Crear cuenta</v-btn>
     </v-form>
@@ -33,6 +41,8 @@
 </template>
 
 <script>
+import {required, email, sameAs, helpers} from 'vuelidate/lib/validators'
+const passRegEx = helpers.regex('passRegEx', /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)
 import UserService from '@/services/UserService'
 
 export default {
@@ -42,19 +52,50 @@ export default {
         mail: null,
         password: null
       },
+      repeatPassword: null,
       result: null
+    }
+  },
+  validations: {
+    user: {
+      mail: { required, email },
+      password: { required, passRegEx }
+    },
+    repeatPassword: {
+      required,
+      sameAsPassword: sameAs('user.password')
     }
   },
   methods: {
     async register () {
-      try {
-        this.result = (await UserService.register(this.user)).data
+      this.$v.$touch()
 
-        this.user.mail = null
-        this.user.password = null
-      } catch (error) {
-        this.result = {error: error}
+      if (this.$v.$invalid){
+        this.result = {
+          invalid: true,
+          message: []
+        }
+        if (!this.$v.user.mail.required ||
+            !this.$v.user.password.required ||
+            !this.$v.repeatPassword.required)
+          this.result.message.push("Todos los campos son obligatorios")
+        if (!this.$v.user.mail.email)
+          this.result.message.push("Debes introducir una dirección de correo válida") 
+        if (!this.$v.user.password.passRegEx)
+          this.result.message.push("La contraseña debe contener al menos una letra, un número y un símbol especial, y tener al menos ocho carácteres de longitud")
+        if (!this.$v.repeatPassword.sameAsPassword)
+          this.result.message.push('Las contraseñas no coinciden')
+      } else {
+        try {
+          this.result = (await UserService.register(this.user)).data
+
+          this.user.mail = null
+          this.user.password = null
+        } catch (error) {
+          this.result = {error: error}
+        }
       }
+      
     }
   },
   mounted () {
@@ -65,7 +106,3 @@ export default {
   }
 }
 </script>
-
-<style>
-
-</style>
