25990517a993506a4fe9e3fe03404a2b6f4a30d8
Alejandro <rs.alejandro.93@gmail.com>
2020-05-20 10:50:36 +0200

    Moved Vuex store to single folder

--- client/src/main.js
+++ client/src/main.js
@@ -2,76 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
-import Vuex from 'vuex'
+import store from './store'
 import VueCookies from 'vue-cookies'
 import Vuelidate from 'vuelidate'
 
-import AuthenticationService from '@/services/AuthenticationService'
-
 Vue.config.productionTip = false
-Vue.use(Vuex)
 Vue.use(VueCookies)
 Vue.use(Vuelidate)
-
-const store = new Vuex.Store({
-  state: {
-    user: null,
-    route: null,
-    transaction: {
-      step: 1,
-      mail: null,
-      price: 0,
-      seats: []
-    }
-  },
-  mutations: {
-    updateUser (state) {
-      AuthenticationService.getUserData()
-        .then(user => {
-          state.user = user.data.name ? user.data.name : user.data.mail
-        })
-        .catch(() => {
-          state.user = null
-        })
-    },
-
-    updatePrice (state, price) {
-      state.transaction.price = price
-    },
-
-    updateMail (state, mail) {
-      state.transaction.mail = mail
-    },
-
-    updateRoute (state, route) {
-      state.route = route
-    },
-
-    addSeat (state, seat) {
-      state.transaction.seats.push(seat)
-    },
-
-    removeSeat (state, id) {
-      state.transaction.seats = state.transaction.seats.filter(seat => seat.id != id)
-    },
-
-    stepUp (state) {
-      state.transaction.step += 1
-    },
-    stepDown (state) {
-      state.transaction.step -= 1
-    },
-
-    resetTransaction (state) {
-      state.transaction = {
-        step: 1,
-        mail: null,
-        price: 0,
-        seats: []
-      }
-    }
-  }
-})
 
 new Vue({
   router,
