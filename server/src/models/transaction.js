aa0a150e50fda2b9376403fb436b83ceff975677
Alejandro <rs.alejandro.93@gmail.com>
2020-05-20 09:06:31 +0200

    Added column for guest users in Transaction model

--- server/src/models/transaction.js
+++ server/src/models/transaction.js
@@ -1,6 +1,17 @@
 module.exports = (sequelize, DataTypes) => {
   const Transaction = sequelize.define('Transaction', {
-    total: DataTypes.DECIMAL(10,2)
+    total: DataTypes.DECIMAL(10,2),
+    guest: {
+      type: DataTypes.STRING,
+      isEmail: true
+    }
+  }, {
+    validate: {
+      guestOrLogged () {
+        if ( (this.guest === null) === (this.UserId === null))
+          throw new Error('One and only one type of user is required.')
+      }
+    }
   })
 
   Transaction.associate = (models) => {
