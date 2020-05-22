66bf9d36d2647759769a699dc68b1eeb6a95dfba
Alejandro <rs.alejandro.93@gmail.com>
2020-05-22 12:18:12 +0200

    Added action buttons

--- client/src/views/AdminZone/UserList.vue
+++ client/src/views/AdminZone/UserList.vue
@@ -8,6 +8,30 @@
 
       <template v-slot:item.name="{ item }"> {{ item.name || 'N/A' }}</template>
 
+      <template v-slot:item.actions="{ item }">
+
+        <v-tooltip top>
+          <template v-slot:activator="{ on }">
+            <v-btn icon color="error" :disabled="item.role == 'ADMIN'" v-on="on">
+              <v-icon>block</v-icon>
+            </v-btn>
+          </template>
+          
+          <span>Bloquear usuario</span>
+        </v-tooltip>
+
+        <v-tooltip top>
+          <template v-slot:activator="{ on }">
+            <v-btn icon color="success" :disabled="item.role == 'ADMIN'" v-on="on">
+              <v-icon>add</v-icon>
+            </v-btn>
+          </template>
+          
+          <span>Hacer administrador</span>
+        </v-tooltip>
+            
+      </template>
+      
     </v-data-table>
   </v-container>
 </template>
