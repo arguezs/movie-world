bb69a69e58b2a0959b95b5a8c071a35fa808490d
Alejandro <rs.alejandro.93@gmail.com>
2020-05-22 12:40:50 +0200

    User list added to admin header

--- client/src/components/header/AdminHeader.vue
+++ client/src/components/header/AdminHeader.vue
@@ -3,17 +3,17 @@
     <v-toolbar-title class="mr-6">Administración</v-toolbar-title>
     <v-toolbar-items>
       <v-btn
-        text
-        exact
+        text exact
         :to="{name: 'Movies'}">Películas</v-btn>
       <v-btn
-        text
-        exact
+        text exact
         :to="{name: 'Sessions'}">Sesiones</v-btn>
       <v-btn
-        text
-        exact
+        text exact
         :to="{name: 'Movie Stats'}">Estadísticas</v-btn>
+      <v-btn
+        text exact
+        :to="{name: 'User List'}">Usuarios</v-btn>
     </v-toolbar-items>
   </v-toolbar>
 </template>
