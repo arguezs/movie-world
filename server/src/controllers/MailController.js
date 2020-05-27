bc55df21126c2475a0192a83e3be6b9b3b5e8945
Alejandro <rs.alejandro.93@gmail.com>
2020-05-27 17:17:38 +0200

    Modified mail aspect

--- server/src/controllers/MailController.js
+++ server/src/controllers/MailController.js
@@ -18,29 +18,29 @@ module.exports = {
 
     const seats = transaction.Seats
 
-    basicMail.html = `<div>
-                        <h1>Movie World</h1>
-                      
-                        <div style="font-size: large;">Gracias por tu compra</div>
-                        <span style="margin-top: 5px;">Esto es un recibo por tu compra ${transaction.id} en Movie World</span>
-
-                        <table>
-                          <tr>
-                            <td>Película</td>
-                            <td>${transaction.Session.Movie.title}</td>
-                          </tr>
-                          <tr>
-                            <td>Fecha</td>
-                            <td>${transaction.Session.date}</td>
-                          </tr>
-                          <tr>
-                            <td>Hora</td>
-                            <td>${transaction.Session.time}</td>
-                          </tr>
-                          <tr style="vertical-align: top;">
-                            <td rowspan="${seats.length}">Asientos</td>
-                            <td>${seats[0].Row.row}-${seats[0].seat}</td>
-                          </tr>`
+    basicMail.html = `<div style="text-align: center; background-color: #f2f2f2; padding: 2vh; margin: 2vh">
+      <h1>Movie <span style="color: purple;">World</span></h1>
+    
+      <div style="font-size: large;">Gracias por tu compra</div>
+      <span style="margin-top: 5px;">Esto es un recibo por tu compra ${transaction.id} en Movie World</span>
+    
+      <table style="margin: 2.5vh auto; border-spacing: 2vw 0; font-size: medium;">
+        <tr>
+          <td style="font-weight: bold;">Película</td>
+          <td>${transaction.Session.Movie.title}</td>
+        </tr>
+        <tr>
+          <td style="font-weight: bold;">Fecha</td>
+          <td>${transaction.Session.date}</td>
+        </tr>
+        <tr>
+          <td style="font-weight: bold;">Hora</td>
+          <td>${transaction.Session.time}</td>
+        </tr>
+        <tr style="vertical-align: top;">
+          <td style="font-weight: bold;" rowspan="${seats.length}">Asientos</td>
+          <td>${seats[0].Row.row}-${seats[0].seat}</td>
+        </tr>`
 
     for (let i=1; i<seats.length; i++) {
       basicMail.html = basicMail.html + `<tr><td>${seats[i].Row.row}-${seats[i].seat}</td></tr>`
