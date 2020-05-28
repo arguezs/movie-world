c45de11534abe7022335cfdddb9ed82e99c4c014
Alejandro <rs.alejandro.93@gmail.com>
2020-05-28 11:57:24 +0200

    Basic validation and functionality for password recovery

--- client/src/views/Session/PasswordRecovery.vue
+++ client/src/views/Session/PasswordRecovery.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-    <v-card max-width="30vw" class="mx-auto my-6">
+    <v-card max-width="30rem" class="mx-auto my-6">
       <v-card-title>Recuperar contraseña</v-card-title>
 
       <v-card-subtitle>Introduce tu dirección de correo y te mandaremos un enlace para reestablecer tu contraseña.</v-card-subtitle>
@@ -10,17 +10,37 @@
       </v-card-text>
 
       <v-card-actions>
-        <v-btn class="mx-auto mb-3">Enviar</v-btn>
+        <v-btn
+          class="mx-auto mb-3"
+          :disabled="invalidMail"
+          color="primary"
+          @click="sendRecoveryCode">Enviar</v-btn>
       </v-card-actions>
     </v-card>
   </v-container>
 </template>
 
 <script>
+import {required, email} from 'vuelidate/lib/validators'
+
 export default {
   data () {
     return {
       mail: null
+    }
+  },
+  validations: {
+    mail: { required, email }
+  },
+  computed: {
+    invalidMail () {
+      this.$v.$touch()
+      return this.$v.$invalid
+    }
+  },
+  methods: {
+    sendRecoveryCode () {
+      
     }
   }
 }
