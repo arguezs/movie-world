de1a1a39487550cfdc6245de11320c33bd64efa3
Alejandro <rs.alejandro.93@gmail.com>
2020-05-30 16:48:27 +0200

    Updated color palette and added new favicon

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -1,7 +1,18 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 
+import colors from 'vuetify/lib/util/colors'
+
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: colors.purple.lighten2, // #9575CD
+        secondary: colors.red.lighten4, // #FFCDD2
+        accent: colors.indigo.base, // #3F51B5
+      },
+    },
+  },
 });
