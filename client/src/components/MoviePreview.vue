cc24c82800afa31e964c232e6873f52aafa6059b
Alejandro <rs.alejandro.93@gmail.com>
2020-05-31 11:59:15 +0200

    Modified MoviePreview aspect

--- client/src/components/MoviePreview.vue
+++ client/src/components/MoviePreview.vue
@@ -1,18 +1,25 @@
 <template>
-  <v-card
-    width="200">
-    <v-img
-      class="white--text align-end"
-      height="300px"
-      :src="`http://localhost:8081/${movie.poster}`" />
-    <v-btn
-      small
-      text
-      block
-      :to="{name:'SingleMovie', params: {movieId: movie.id}}">
-      <span class="text-truncate">{{movie.title}}</span>
-    </v-btn>
-  </v-card>
+
+  <v-hover>
+    <template v-slot="{ hover }">
+      <v-card width="200">
+        <v-img
+          class="white--text align-end"
+          height="300px"
+          :src="`http://localhost:8081/${movie.poster}`" />
+
+        <v-fade-transition>
+          <v-overlay v-if="hover" absolute>
+            <v-btn text small
+              :to="{name:'SingleMovie', params: {movieId: movie.id}}">
+              <span class="text-truncate">{{movie.title}}</span>
+            </v-btn>
+          </v-overlay>
+        </v-fade-transition>
+      </v-card>
+    </template>
+  </v-hover>
+  
 </template>
 
 <script>
@@ -24,7 +31,5 @@ export default {
 </script>
 
 <style scoped>
-.text-truncate{
-  max-width: 200px;
-}
+.text-truncate{max-width: 200px;}
 </style>
