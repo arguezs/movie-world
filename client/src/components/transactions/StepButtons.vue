5b1cf85fbea70ad65c3712b1d08f3a859ed036ac
Alejandro <rs.alejandro.93@gmail.com>
2020-06-06 13:36:54 +0200

    Responsive adaptation

--- client/src/components/transactions/StepButtons.vue
+++ client/src/components/transactions/StepButtons.vue
@@ -1,10 +1,11 @@
 <template>
-  <v-row class="mt-6" justify="end">
-      <v-col cols="auto">
+  <v-row class="mt-6"
+    :justify="($vuetify.breakpoint.xs || $vuetify.breakpoint.sm) ? 'center' : 'end'">
+      <v-col cols="auto" order="2" order-md="1">
         <v-btn ref="prevStep" @click="prevStep">Cancelar</v-btn>
       </v-col>
 
-      <v-col cols="auto">
+      <v-col cols="auto" order="1" order-md="2">
         <v-btn ref="nextStep" color="accent" @click="nextStep" :disabled="disable">Continuar</v-btn>
       </v-col>
     </v-row>
